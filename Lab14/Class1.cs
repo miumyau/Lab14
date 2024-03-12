da82e104242d33d3ceea135618ad2e00b8586661
ArtiOnMoon <67146003+ArtiOnMoon@users.noreply.github.com>
2024-03-12 17:55:28 +0300

    123

---
+++ Lab14/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+        public  void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+
+    class Cat:Animal
+    {
+        private string name;
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Cat (string name)
+            :base(name)
+        {
+            Name = name;
+        }
+
+            public override void Say()
+        {
+            Console.WriteLine ("Мяу:3"); ;
+        }
+    }
+
+    class Dog : Animal
+    {
+        private string name;
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Dog (string name)
+            : base(name)
+        {
+            Name = name;
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Гав:3"); ;
+        }
+    }
+
+}
--- Lab14/Program.cs
+++ Lab14/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace Lab14
 {
     internal class Program
+
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Cat cat = new Cat("бебрик");
+            Dog dog = new Dog("песя");
+            cat.Say();
+            dog.Say();
+            cat.ShowInfo();
+            dog.ShowInfo();
+            Console.ReadKey();
         }
     }
 }
